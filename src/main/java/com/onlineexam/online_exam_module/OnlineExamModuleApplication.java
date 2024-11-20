fdb18ee7776c4d440671e107f0d74fe927c2c8b7
Chintan Gelot <chintan3701@gmail.com>
2024-11-21 00:38:02 +0530

    Initial springboot setup

---
+++ src/main/java/com/onlineexam/online_exam_module/OnlineExamModuleApplication.java
@@ -0,0 +1,13 @@
+package com.onlineexam.online_exam_module;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineExamModuleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlineExamModuleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/onlineexam/online_exam_module/OnlineExamModuleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.onlineexam.online_exam_module;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineExamModuleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
